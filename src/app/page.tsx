d08ec4d1800e12650becfe114abff8ad60bc0711
JoonasMV <JoonasMRViljanen@gmail.com>
2025-03-12 07:57:04 +0200

    Update home; add routes for future pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,24 @@
+import Link from "next/link";
+
 const Home = async () => {
   return (
     <main className="home-page">
       <div>
-        <h2>Asukastoimikuntaopas</h2>
-        <a href="https://hoas.fi/app/uploads/2024/01/Asukastoimintaopas.pdf">
-          Asukastoimikuntaopas
-        </a>
-      </div>
-
-      {/* Kokoukset ja tapahtumat */}
-      <div>
         <h2>Kokoukset ja tapahtumat</h2>
-        <a href="https://hoas.fi/app/uploads/2021/06/Asukastoimikunnan-kokouskutsu-FI-EN.pdf">
-          Kokouskutsu
-        </a>
-        <a href="https://hoas.fi/app/uploads/2024/01/Asukastoimikuntien-kokouspoytakirja-FIN.pdf">
-          Kokouspöytäkirja
-        </a>
-        <a href="https://hoas.fi/app/uploads/2021/06/Tapahtumien-osallistujalista-FI-EN.pdf">
-          Tapahtumien osallistujalista
-        </a>
+        <Link href={"/meeting-invite"}>Kokouskutsu</Link>
+        <Link href={"/minutes"}>Kokouspöytäkirja</Link>
       </div>
 
-      {/* Yhteistilat ja tavarat */}
       <div>
         <h2>Yhteistilat ja tavarat</h2>
-        <a href="https://hoas.fi/app/uploads/2024/01/Asukastoimikuntien-avainlainalomake-2.pdf">
-          Avainlomake
-        </a>
-        <a href="https://hoas.fi/app/uploads/2024/11/Tavarainventaario-FI-EN.xlsx">
-          Tavarainventaario
-        </a>
-        <a href="https://hoas.fi/app/uploads/2021/06/Avain-ja-tilainventaario-FI-EN.pdf">
-          Avain- ja tilainventaario
-        </a>
+        <Link href={"/inventory"}>Tavarainventaario</Link>
       </div>
 
-      {/* Määrärahat ja tilitykset */}
       <div>
         <h2>Määrärahat ja tilitykset</h2>
-        <a href="https://hoas.fi/app/uploads/2021/06/Tilityslomake-FI.pdf">
-          Tilityslomake
-        </a>
-        <a href="https://hoas.fi/app/uploads/2021/06/Asukastoimikunnan-korvausvastuuhinnasto-FI-EN.pdf">
-          Asukastoimikunnan korvausvastuuhinnasto
-        </a>
+        <Link href={"/accounting"}>Tilityslomake</Link>
       </div>
 
-      {/* Alkuvuoden materiaalit */}
       <div>
         <h2>Alkuvuoden materiaalit</h2>
         <a href="https://hoas.fi/app/uploads/2024/01/ASUKASKOKOUSPOYTAKIRJA.pdf">
@@ -55,6 +26,22 @@ const Home = async () => {
         </a>
         <a href="https://hoas.fi/app/uploads/2021/06/Asukaskokouksen-osallistuja-ja-yhteystietolomake-FI-EN.pdf">
           Asukaskokoukset osallituja- ja yhteystietolomake
+        </a>
+      </div>
+
+      <div>
+        <h2>Dokumentit</h2>
+        <a href="https://hoas.fi/app/uploads/2024/01/Asukastoimintaopas.pdf">
+          Asukastoimikuntaopas
+        </a>
+        <a href="https://hoas.fi/app/uploads/2021/06/Tapahtumien-osallistujalista-FI-EN.pdf">
+          Tapahtumien osallistujalista
+        </a>
+        <a href="https://hoas.fi/app/uploads/2024/01/Asukastoimikuntien-avainlainalomake-2.pdf">
+          Avainlomake
+        </a>
+        <a href="https://hoas.fi/app/uploads/2021/06/Asukastoimikunnan-korvausvastuuhinnasto-FI-EN.pdf">
+          Asukastoimikunnan korvausvastuuhinnasto
         </a>
       </div>
     </main>
