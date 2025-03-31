83aefc0c0b346ce965d8f778c3f6acb6b70b15d5
JoonasMV <JoonasMRViljanen@gmail.com>
2025-03-31 14:21:35 +0300

    Add DownloadPdf function

---
+++ src/app/components/DownloadPdf.ts
@@ -0,0 +1,22 @@
+import { pdf } from "@react-pdf/renderer";
+
+export const DownloadPdf = async (
+  filename: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  pdfElement: React.ReactElement<any>
+) => {
+  const blob = await pdf(pdfElement).toBlob();
+
+  const url = URL.createObjectURL(blob);
+
+  // Create temporary <a/> element to download the pdf
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `${filename}.pdf`;
+  document.body.appendChild(a);
+
+  a.click();
+
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+};
