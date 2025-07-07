d55e384f1f173ea37bd537360be20565d6f04a0b
joovil <JoonasMRViljanen@gmail.com>
2025-07-07 16:45:39 +0300

    Add DynamicKeyValList component for managing name-price pairs in accounting

--- src/app/accounting/page.tsx
+++ src/app/accounting/page.tsx
@@ -1,11 +1,54 @@
 "use client";
 
+import React, { ChangeEvent, useRef, useState } from "react";
+
+interface Acquisition {
+  name: string;
+  price: number;
+}
+
 const Accounting = () => {
+  const [newPurchase, setNewPurchase] = useState<Acquisition>({
+    name: "",
+    price: 0,
+  });
+  const [purchases, setPurchases] = useState<Acquisition[]>([]);
+  const focusRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const val = e.currentTarget.value;
+    const key = e.currentTarget.name;
+
+    if (!key || !val) return;
+
+    setNewPurchase((prev) => ({ ...prev, [key]: val }));
+  };
+
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setPurchases((prev) => [...prev, newPurchase]);
+      setNewPurchase({ name: "", price: 0 });
+      focusRef.current?.focus();
+    }
+  };
+
+  const handleUpdate = (e: ChangeEvent<HTMLInputElement>, i: number) => {
+    const key = e.currentTarget.name;
+    const val = e.currentTarget.value;
+
+    const updated = [...purchases];
+    const update: Acquisition = { ...purchases[i], [key]: val };
+
+    updated[i] = update;
+
+    setPurchases(updated);
+  };
+
   return (
-    <div className="flex flex-col gap-lg bg-pin">
+    <div className="gap-lg bg-pin flex flex-col">
       <h1>Tilityslomake</h1>
 
-      <div className="flex flex-col gap-1 [&>div]:flex [&_input]:w-full">
+      <div className="flex flex-col gap-1 [&_input]:w-full [&>div]:flex">
         <h2>Tilityslomake</h2>
 
         <div className="flex">
@@ -62,24 +105,69 @@ const Accounting = () => {
         <div>Tililaji 707: kaikki alkoholit</div>
       </div>
 
+      {/* TEST */}
+      {/* <DynamicKeyValList<Acquisition> /> */}
+      {/* TEST */}
+
       {/* TILILAJI GRID */}
       <div>
-        <div className="grid grid-cols-[1fr_4fr_1fr] gap-[1px] bg-white">
-          <div className="bg-pink-vehka text-white pl-2">TILILAJI</div>
-          <div className="bg-pink-vehka text-white pl-2">
+        <div className="grid grid-cols-[1fr_3fr_1fr] gap-[1px] bg-white">
+          <div className="bg-pink-vehka pl-2 text-white">TILILAJI</div>
+          <div className="bg-pink-vehka pl-2 text-white">
             HANKINNAT KUITTIEN MUKAAN
           </div>
-          <div className="bg-pink-vehka text-white flex justify-center">€</div>
+          <div className="bg-pink-vehka flex justify-center text-white">€</div>
         </div>
 
-        <div className="grid grid-cols-[1fr_4fr_1fr] gap-[1px] bg-black [&>div]:bg-white [&>div]:pl-1 border-x-1 border-b-1">
-          <div>TEST</div>
-          <div>TEST</div>
-          <div>TEST</div>
+        <div className="grid grid-cols-[1fr_3fr_1fr] gap-[1px] border-x-1 border-b-1 bg-black [&>div]:bg-white [&>div]:pl-1 [&>input]:w-full">
+          {/* New item */}
+          <div>701 hankinnat</div>
+          <input
+            ref={focusRef}
+            type="text"
+            name="name"
+            value={newPurchase.name}
+            onChange={(e) => handleChange(e)}
+            onKeyDown={(e) => handleEnter(e)}
+          />
+          <input
+            type="number"
+            name="price"
+            value={newPurchase.price}
+            onChange={(e) => handleChange(e)}
+            onKeyDown={(e) => handleEnter(e)}
+          />
 
-          <div>TEST</div>
-          <div>TEST</div>
-          <div>TEST</div>
+          {/* Edit item */}
+          {purchases.map((p, i) => (
+            <React.Fragment key={i}>
+              <div>701 hankinnat</div>
+              <input
+                type="text"
+                name="name"
+                value={purchases[i].name}
+                onChange={(e) => handleUpdate(e, i)}
+              />
+              <input
+                type="number"
+                name="price"
+                value={purchases[i].price}
+                onChange={(e) => handleUpdate(e, i)}
+              />
+            </React.Fragment>
+          ))}
+
+          {/* <div>704 virkistys</div>
+          <input type="text" />
+          <input type="number" />
+
+          <div>707 alkoholi</div>
+          <input type="text" />
+          <input type="number" />
+
+          <div>Epäselvät</div>
+          <input type="text" />
+          <input type="number" /> */}
         </div>
       </div>
     </div>
