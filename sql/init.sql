6feff9151bc717d09fe0c22bc7062844db9f2d6a
joovil <JoonasMRViljanen@gmail.com>
2025-08-06 13:58:36 +0300

    Refactor database schema and types: rename fields for consistency and remove unused tables

--- sql/init.sql
+++ sql/init.sql
@@ -6,23 +6,5 @@ CREATE TABLE minutes (
 	id SERIAL PRIMARY KEY,
 	blob_url TEXT,
 	minutes_number INTEGER,
-	createad_date DATE
-);
-
-CREATE TABLE expenses (
-	id SERIAL PRIMARY KEY,
-	blob_url TEXT,
-	amount NUMERIC(10, 2)
-);
-
-CREATE TABLE minutes_expenses (
-	id SERIAL PRIMARY KEY,
-	minutes_id INTEGER REFERENCES minutes(id),
-	expenses_id INTEGER REFERENCES expenses(id)
-);
-
-CREATE TABLE receipts (
-	id SERIAL PRIMARY KEY,
-	blob_url TEXT,
-	expense_id 	INTEGER REFERENCES expenses(id)
+	created_date DATE
 );
