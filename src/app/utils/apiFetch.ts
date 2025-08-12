9cdf14c9de4b2a3fe668090dd8cf9b11dd147b8d
joovil <JoonasMRViljanen@gmail.com>
2025-08-12 09:39:20 +0300

    feat(api): implement apiFetch utility for API requests

---
+++ src/app/utils/apiFetch.ts
@@ -0,0 +1,10 @@
+export const apiFetch = async (
+  endpoint: RequestInfo | URL,
+  options: RequestInit,
+): Promise<Response> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api${endpoint}`,
+    options,
+  );
+  return res;
+};
