7e8e39d182f4d4cd1c8e857e64b45543d72bae73
joovil <JoonasMRViljanen@gmail.com>
2025-08-12 14:14:30 +0300

    feat(database): update schema for minutes and tenant committees; add foreign key relationship feat(tests): implement unit tests for minutes repository functions chore(tests): setup environment configuration and database cleanup for tests

--- src/server/db/repos/minutesRepo.ts
+++ src/server/db/repos/minutesRepo.ts
@@ -1,5 +1,42 @@
+import { sql } from "kysely";
 import { db } from "../database";
 
 export const getMinutes = async () => {
   return await db.selectFrom("minutes").selectAll().execute();
+};
+
+export const addMinutes = async ({
+  filename,
+  blobUrl,
+  tenantCommitteeId,
+}: {
+  filename: string;
+  blobUrl: string;
+  tenantCommitteeId: number;
+}) => {
+  await db.transaction().execute(async (trx) => {
+    const year = new Date().getFullYear();
+
+    const [{ count }] = await trx
+      .selectFrom("minutes")
+      .select((eb) => eb.fn.count("id").as("count"))
+      .where("tenantCommitteeId", "=", tenantCommitteeId)
+      .where(sql`EXTRACT(YEAR FROM "created")`, "=", year)
+      .execute();
+
+    const entryNumber = Number(count) + 1;
+
+    const newEntry = await trx
+      .insertInto("minutes")
+      .values({
+        tenantCommitteeId: tenantCommitteeId,
+        filename,
+        blobUrl,
+        number: `${entryNumber}/${year}`,
+        created: new Date(),
+      })
+      .executeTakeFirstOrThrow();
+
+    return newEntry;
+  });
 };
